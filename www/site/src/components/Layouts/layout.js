f11037be086c8c22e25950a25cfc5f6af29586ee
talves <tony@3alves.com>
2020-02-28 18:11:51 -0800

    add layout

--- www/site/src/components/Layouts/layout.js
+++ www/site/src/components/Layouts/layout.js
@@ -39,7 +39,39 @@ export const Layout = ({ children, header = 'Header' }) => {
         </Box>
       </Header>
       <Main variant="styles.Layout">
-        <Container>{children}</Container>
+        <Container>
+          <Box
+            sx={{
+              fontFamily: 'body',
+              '& p': { margin: '5px 0 5px 10px', display: 'block' },
+              '& pre': {
+                background: '#f4f4f4',
+                border: '1px solid #ddd',
+                borderLeft: '3px solid #f36d33',
+                color: '#666',
+                pageBreakInside: 'avoid',
+                fontFamily: 'Menlo,monospace',
+                fontSize: '15px',
+                lineHeight: '1em',
+                marginBottom: '1.6em',
+                marginLeft: '30px',
+                maxWidth: '100%',
+                overflow: 'auto',
+                overflowX: 'auto',
+                padding: '0.5em 1em',
+                display: 'block',
+                wordWrap: 'break-word',
+                whiteSpace: 'pre-wrap',
+                counterReset: 'line',
+              },
+              '& pre > code': {
+                position: 'relative',
+              },
+            }}
+          >
+            {children}
+          </Box>
+        </Container>
       </Main>
     </FlexLayout>
   );
